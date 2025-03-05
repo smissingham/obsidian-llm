3289e625e20b65eddfdf42cb704b0cddeddd7b32
Sean Missingham <sean@missingham.com>
2025-03-04 20:23:35 -0600

    Install TailwindCSS 4.0.9

---
+++ src/views/ReactView.tsx
@@ -0,0 +1,35 @@
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import { StrictMode } from "react";
+import { Root, createRoot } from "react-dom/client";
+
+export const VIEW_TYPE_EXAMPLE = "example-view";
+
+export class ExampleView extends ItemView {
+	root: Root | null = null;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Example view";
+	}
+
+	async onOpen() {
+		this.root = createRoot(this.containerEl.children[1]);
+		this.root.render(
+			<StrictMode>
+				<h1>Hello World</h1>
+				<p className="text-blue-500">This is a test</p>
+			</StrictMode>
+		);
+	}
+
+	async onClose() {
+		this.root?.unmount();
+	}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/styles/main.css"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
